74d1a8507f24d721cfe68d138f59372e75d0fca8
spinyo35 <38720867+spinyo35@users.noreply.github.com>
2023-07-21 12:18:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/member.html',
+        controller: 'MemberController',
+        controllerAs: 'memberCtrl'
+    };
+    }   
+
